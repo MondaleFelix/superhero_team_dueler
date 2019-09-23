b507e8ed452b7f15df3bd23fe8462d0199ebd502
MondaleFelix <mondalefelix@yahoo.com>
2019-09-23 15:02:54 -0700

    added template

---
+++ superheros.py
@@ -0,0 +1,27 @@
+class Ability:
+
+	def __init__(self, max_damage)
+		self.max_damage = max_damage
+
+	def attack():
+		pass
+
+
+class Armor:
+
+	def __init__(self, name, max_block)
+		self.name = name
+		self.max_block = max_block
+
+	def block():
+		pass
+
+
+class Hero:
+
+	def __init__(name, starting_health):
+		self.name = name
+		self.starting_health = 100
+
+	def add_ability(Ability):
+		pass
